6fd3b8f9cbaa5af4b0d27623f70f4b07548e666b
Lekshmi Rajappa <mail2leks@gmail.com>
2020-03-16 00:51:03 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/MainComponent.js';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
+
+const store = ConfigureStore();
+
+class App extends Component{
+
+render(){
+  return(
+    <Provider store={store}>
+    <Router>
+    <div className="App">
+     <Main/>
+    </div>
+    </Router>
+    </Provider>);
+}
+}
+export default App;
