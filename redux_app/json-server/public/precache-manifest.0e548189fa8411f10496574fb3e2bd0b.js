f8541be4d87f4f4ac39e48a43024dab4bebc1b1d
Lekshmi Rajappa <mail2leks@gmail.com>
2020-06-06 00:38:48 -0700

    deployment

---
+++ redux_app/json-server/public/precache-manifest.0e548189fa8411f10496574fb3e2bd0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbe66ac31a65a34fb52af03e7c4a9b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "600fcc93d0994aa8f7ec",
+    "url": "/static/css/main.b430ae83.chunk.css"
+  },
+  {
+    "revision": "5368283d58fba44cc2bd",
+    "url": "/static/js/2.b77a7e8f.chunk.js"
+  },
+  {
+    "revision": "d48f557b335fe40c661607e7b46704c8",
+    "url": "/static/js/2.b77a7e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600fcc93d0994aa8f7ec",
+    "url": "/static/js/main.a30e6272.chunk.js"
+  },
+  {
+    "revision": "0c957624af32c495ebd0",
+    "url": "/static/js/runtime-main.a06492e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
