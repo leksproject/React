6a865d41bc16ad1fc28be2c775305e2da0bd3bf7
Lekshmi Rajappa <mail2leks@gmail.com>
2020-06-06 11:53:01 -0700

    Updates

---
+++ precache-manifest.cf11323e00f90bc5a8ac7b20c5cc90d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3110c0e7be30feadad93e0d8da3a50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f336f8e315359fe2621",
+    "url": "/static/css/main.b430ae83.chunk.css"
+  },
+  {
+    "revision": "3121dc1204ab0e9b2eba",
+    "url": "/static/js/2.07c7279b.chunk.js"
+  },
+  {
+    "revision": "d48f557b335fe40c661607e7b46704c8",
+    "url": "/static/js/2.07c7279b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f336f8e315359fe2621",
+    "url": "/static/js/main.bdac6eb2.chunk.js"
+  },
+  {
+    "revision": "0c957624af32c495ebd0",
+    "url": "/static/js/runtime-main.a06492e9.js"
+  }
+]);
